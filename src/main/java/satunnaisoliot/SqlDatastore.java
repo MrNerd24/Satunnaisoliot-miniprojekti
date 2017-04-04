8e3a74dae0c16a56bc5e001a3c066411559228c2
Juuso Lassila <juuso.lassila@helsinki.fi>
2017-04-04 16:48:43 +0300

    Mergetty tietorakenteet

--- src/main/java/satunnaisoliot/SqlDatastore.java
+++ src/main/java/satunnaisoliot/SqlDatastore.java
@@ -48,6 +48,8 @@ public class SqlDatastore {
         }
         return null;
     }
+    
+    public Connection getConnection() { return this.connection; }
 
     public String getFilename() {
         return this.filename;
