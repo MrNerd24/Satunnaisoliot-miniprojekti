1bbf066cf74fb02a2cd6514a08eb5531596e6246
Juuso Lassila <juuso.lassila@helsinki.fi>
2017-04-05 19:20:34 +0300

    Bibtex Generointi aloitettu

--- src/main/java/satunnaisoliot/datastructures/generic/GenericReference.java
+++ src/main/java/satunnaisoliot/datastructures/generic/GenericReference.java
@@ -1,11 +1,11 @@
 package satunnaisoliot.datastructures.generic;
 
+import java.util.ArrayList;
 import satunnaisoliot.datastructures.enums.FieldType;
 import satunnaisoliot.datastructures.interfaces.Reference;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Objects;
-
 
 /**
  *
@@ -14,6 +14,15 @@ import java.util.Objects;
 public abstract class GenericReference implements Reference {
 
     EnumMap<FieldType, String> entries = new EnumMap<FieldType, String>(FieldType.class);
+    String bibTexKey;
+
+    public String getBibTexKey() {
+        return bibTexKey;
+    }
+
+    public void setBibTexKey(String bibTexKey) {
+        this.bibTexKey = bibTexKey;
+    }
 
     @Override
     public void setField(FieldType name, String value) {
@@ -50,12 +59,48 @@ public abstract class GenericReference implements Reference {
 
     @Override
     public List<String> toBibTex() {
-        return null;
+        if (getBibTexKey() == null) {
+            throw new RuntimeException("BibTex key was null when trying to generate Bibtex");
+        }
+        ArrayList<String> lines = new ArrayList<String>();
+
+        String type = getType().toString();
+        String start = "@" + type + "{" + getBibTexKey() + ",";
+        lines.add(replaceBibTexSpecialChars(start));
+        
+        for (FieldType field : FieldType.values()) {
+            String content = getField(field);
+            if (content == null) {
+                continue;
+            }
+            String[] values = content.split(";");
+            
+            String value = values[0];
+            for (int i = 1; i < values.length; i++) {
+                value = value + " and " + values[i] ;
+            }
+            value = replaceBibTexSpecialChars(value);
+            lines.add(field.toString() + " = \"" + value + "\"");
+        }
+        lines.add("}");
+        return lines;
     }
 
     @Override
     public String toString() {
-        return "GenericReference{" + '}';
+        return getType().toString() + " " + getBibTexKey();
     }
-    
+
+    private String replaceBibTexSpecialChars(String input) {
+        input = input.replaceAll("ä", "{\\\"a}");
+        input = input.replaceAll("ö", "{\\\"o}");
+        input = input.replaceAll("å", "{\\aa}");
+
+        input = input.replaceAll("Ä", "{\\\"A}");
+        input = input.replaceAll("Ö", "{\\\"O}");
+        input = input.replaceAll("Å", "{\\AA}");
+
+        return input;
+    }
+
 }
