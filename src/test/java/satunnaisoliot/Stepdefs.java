9758ac171429ab5738c2979d5f5cb6e68e0bf34f
Juuso Lassila <juuso.lassila@helsinki.fi>
2017-04-12 11:21:20 +0300

    cucumber yrittämistä

--- src/test/java/satunnaisoliot/Stepdefs.java
+++ src/test/java/satunnaisoliot/Stepdefs.java
@@ -9,41 +9,46 @@ import java.awt.Frame;
 import satunnaisoliot.gui.MainWindow;
 
 public class Stepdefs {
+
     private Frame[] openFrames;
     private MainWindow mw;
-        @Before
+
+    @Before
     public void setUp() {
-      openFrames = MainWindow.getFrames();
-      
+        openFrames = MainWindow.getFrames();
+
     }
 
     @Given("^Program is not running$")
-    public void program_not_running() throws Throwable {
+    public void program_is_not_running() throws Throwable {
         openFrames = MainWindow.getFrames();
-        System.out.println("open frames (not running): "+ openFrames.length);
+        System.out.println("open frames (not running): " + openFrames.length);
         assertFalse(openFrames.length > 1);
     }
+
     @Given("^Program is running$")
     public void program_is_running() throws Throwable {
         String[] arg = {""};
         Main.main(arg);
         Thread.sleep(2500);
         openFrames = MainWindow.getFrames();
-        System.out.println("open frames (running): "+ openFrames.length);
+        System.out.println("open frames (running): " + openFrames.length);
         assertTrue(openFrames.length >= 1);
     }
+
     @When("^User launches the program$")
     public void program_launch() throws Throwable {
         openFrames = MainWindow.getFrames();
-        System.out.println("open frames (launch): "+ openFrames.length);
+        System.out.println("open frames (launch): " + openFrames.length);
     }
+
     @Then("^Main window opens$")
     public void main_window_open() throws Throwable {
         String[] arg = {""};
         Main.main(arg);
         Thread.sleep(2500);
         openFrames = MainWindow.getFrames();
-        System.out.println("open frames (launch): "+ openFrames.length);
+        System.out.println("open frames (launch): " + openFrames.length);
         assertTrue(openFrames.length >= 1);
     }
 }
