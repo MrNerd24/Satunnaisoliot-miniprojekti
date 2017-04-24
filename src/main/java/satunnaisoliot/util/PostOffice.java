2e43bed5b37aac485fadea6d75393d0bd6ee0f64
MrNerd24 <juuso.lassila@helsinki.fi>
2017-04-24 21:59:47 +0300

    Generoidut kommentit poistettu

--- src/main/java/satunnaisoliot/util/PostOffice.java
+++ src/main/java/satunnaisoliot/util/PostOffice.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package satunnaisoliot.util;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- *
- * @author juuso_j0pbwen
- */
 public class PostOffice {
 
     private static HashMap<String, ArrayList<MessageListener>> listeners = new HashMap<>();
